d1fcd1a0b9814d487d761b55136ad9847d1610d0
jayden220 <jaydenvanrooyen87@gmail.com>
2024-06-07 13:32:01 +0200

    first

---
+++ script/script.js
@@ -0,0 +1,19 @@
+let sortBtn = document.querySelectorAll('button')[0]
+let addBtn = document.querySelectorAll('button')[1]
+
+
+
+sortBtn.addEventListener('click', sort)
+
+function sort() {
+    alert('hello')
+}
+
+
+
+
+addBtn.addEventListener('click', add)
+
+function add() {
+    alert('hi')
+}
