930b728159dfe74971cdf11aace6e7b8523358e9
jayden220 <jaydenvanrooyen87@gmail.com>
2024-06-07 15:54:19 +0200

    added script

--- script/script.js
+++ script/script.js
@@ -1,27 +1,52 @@
 let sortBtn = document.querySelectorAll('button')[0]
 let addBtn = document.querySelectorAll('button')[1]
 let delete1 = document.querySelectorAll('button')[2]
-
+let items = document.querySelector('ul')
 let toDo = document.querySelectorAll('input')[0]
 let check1 = document.querySelectorAll('input')[1]
 let check2 = document.querySelectorAll('input')[2]
 let check3 = document.querySelectorAll('input')[3]
 
 
-sortBtn.addEventListener('click', sort)
+let list = []
 
-function sort() {
-    alert('hello')
+function addItems(id,name,date,completion) {
+    this.id = id
+    this.name = name
+    this.date =date
+    this.completion = completion
+}
+
+function addList(){
+
+    let currentDate = new Date().getDay()
+
+    let year =currentDate
+
+    let item = new addItems(1, toDo.value, year,true)
+    list.push(toDo.value)
+
+    if(toDo.value.trim()!== ''){
+        let array = [item.name]
+        for (const arr of array) {
+            
+            items.innerHTML +=  `
+                                <li>
+                                    <input type="checkbox" class="check1"><h4>${arr}</h4><button class="del1">x</button>
+                                </li>        
+                                `
+        }
+        }    
+    }
+
+addBtn.addEventListener('click', enter)
+// toDo.addEventListener('input',addList)
+
+
+
+function enter() {
+    addList()
+    
 }
 
 
-
-
-addBtn.addEventListener('click', add)
-
-function add() {
-    alert('hi')
-}
-
-
-let arrayList = new Array()
