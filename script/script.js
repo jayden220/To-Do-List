b614f6aab2ffd68c2264c5c2cbed38caa96028e4
jayden220 <jaydenvanrooyen87@gmail.com>
2024-06-07 14:14:55 +0200

    second push

--- script/script.js
+++ script/script.js
@@ -1,6 +1,11 @@
 let sortBtn = document.querySelectorAll('button')[0]
 let addBtn = document.querySelectorAll('button')[1]
+let delete1 = document.querySelectorAll('button')[2]
 
+let toDo = document.querySelectorAll('input')[0]
+let check1 = document.querySelectorAll('input')[1]
+let check2 = document.querySelectorAll('input')[2]
+let check3 = document.querySelectorAll('input')[3]
 
 
 sortBtn.addEventListener('click', sort)
@@ -17,3 +22,6 @@ addBtn.addEventListener('click', add)
 function add() {
     alert('hi')
 }
+
+
+let arrayList = new Array()
